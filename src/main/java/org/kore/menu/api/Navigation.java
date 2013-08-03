f75d9fb89afa8233fb52b1ba5ca2ba665269e415
Konrad Renner <koni.renner@gmail.com>
2013-08-03 20:31:02 +0200

    initial setup

---
+++ src/main/java/org/kore/menu/api/Navigation.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kore.menu.api;
+
+/**
+ * Navigation
+ *
+ * Entry-Point for the navigation/menus. This interface has to be
+ * implemented/loaded in the client application of this API
+ *
+ * @author koni
+ */
+public interface Navigation {
+}
